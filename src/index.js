9d95f7eb490b65c822a6f83ff0f1a82cd06f1904
Jonathan Nieves <nieves.jon.8@gmail.com>
2024-07-26 14:07:50 -0400

    add basic temp display functionality

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import WeatherWrapper from './weather-wrapper';
 
 // code
 const weather = new WeatherWrapper(new WeatherAPI());
-
+const tempDisplay = document.querySelector('.focused-weather-pane .temp-container .current-temp-display');
+const minTempDisplay = document.querySelector('.focused-weather-pane .temp-container .min-temp-display');
+const maxTempDisplay = document.querySelector('.focused-weather-pane .temp-container .max-temp-display');
 weather.reload().then(() => {
   let today = weather.week[0];
   let temp = today.temp;
   let maxTemp = today.tempmax;
   let minTemp = today.tempmin;
-  console.log(minTemp, temp, maxTemp);
-})
+
+  tempDisplay.innerText = temp;
+  minTempDisplay.innerText = minTemp;
+  maxTempDisplay.innerText = maxTemp;
+});
