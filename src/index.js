d25fd7a5c90d320f5830a4cb593134d00ed5b108
Jonathan Nieves <nieves.jon.8@gmail.com>
2024-08-07 11:54:34 -0400

    add dynamic weather icons

--- src/index.js
+++ src/index.js
@@ -2,35 +2,51 @@
 import "./style.css";
 import WeatherAPI from "./weather.js";
 import WeatherWrapper from "./weather-wrapper";
-import rainIcon from './icons/rain.png';
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context("./icons", false, /\.(png|jpe?g|svg)$/),
+);
 
 // code
 const weather = new WeatherWrapper(new WeatherAPI());
-weather.reload().then(() => {
-  (function renderToday() {
-    let today = weather.week[0];
-    console.log(today);
 
-    const tempDisplay = document.querySelector(
-      ".focused-weather-pane .temp-container .current-temp-display",
-    );
-    const minTempDisplay = document.querySelector(
-      ".focused-weather-pane .temp-container .min-temp-display",
-    );
-    const maxTempDisplay = document.querySelector(
-      ".focused-weather-pane .temp-container .max-temp-display",
-    );
+function updateDisplay() {
+  weather.reload().then(() => {
+    (function renderToday() {
+      let today = weather.week[0];
+      console.log(today);
 
-    const descriptionDisplay = document.querySelector(
-      ".focused-weather-pane .other-info-container .weather-description",
-    );
+      const tempDisplay = document.querySelector(
+        ".focused-weather-pane .temp-container .current-temp-display",
+      );
+      const minTempDisplay = document.querySelector(
+        ".focused-weather-pane .temp-container .min-temp-display",
+      );
+      const maxTempDisplay = document.querySelector(
+        ".focused-weather-pane .temp-container .max-temp-display",
+      );
 
-    const iconDisplay = document.querySelector(".focused-weather-pane img")
+      const descriptionDisplay = document.querySelector(
+        ".focused-weather-pane .other-info-container .weather-description",
+      );
 
-    iconDisplay.src = rainIcon;
-    descriptionDisplay.innerText = today.description;
-    tempDisplay.innerText = today.temp;
-    minTempDisplay.innerText = today.tempmin;
-    maxTempDisplay.innerText = today.tempmax;
-  })();
-});
+      const iconDisplay = document.querySelector(".focused-weather-pane img");
+
+      iconDisplay.src = images[today.icon + ".png"];
+      descriptionDisplay.innerText = today.description;
+      tempDisplay.innerText = today.temp;
+      minTempDisplay.innerText = today.tempmin;
+      maxTempDisplay.innerText = today.tempmax;
+    })();
+  });
+}
+
+updateDisplay();
