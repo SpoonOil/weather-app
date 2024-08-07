48ff08e8e4ab5d934db26480ba5a1d9aa7e32bc5
Jonathan Nieves <nieves.jon.8@gmail.com>
2024-08-07 02:16:36 -0400

    fiddled with stuff add icons for weather at weather description

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 // imports
-import './style.css';
-import WeatherAPI from './weather.js';
-import WeatherWrapper from './weather-wrapper';
+import "./style.css";
+import WeatherAPI from "./weather.js";
+import WeatherWrapper from "./weather-wrapper";
+import rainIcon from './icons/rain.png';
 
 // code
 const weather = new WeatherWrapper(new WeatherAPI());
-const tempDisplay = document.querySelector('.focused-weather-pane .temp-container .current-temp-display');
-const minTempDisplay = document.querySelector('.focused-weather-pane .temp-container .min-temp-display');
-const maxTempDisplay = document.querySelector('.focused-weather-pane .temp-container .max-temp-display');
 weather.reload().then(() => {
-  let today = weather.week[0];
-  let temp = today.temp;
-  let maxTemp = today.tempmax;
-  let minTemp = today.tempmin;
+  (function renderToday() {
+    let today = weather.week[0];
+    console.log(today);
 
-  tempDisplay.innerText = temp;
-  minTempDisplay.innerText = minTemp;
-  maxTempDisplay.innerText = maxTemp;
+    const tempDisplay = document.querySelector(
+      ".focused-weather-pane .temp-container .current-temp-display",
+    );
+    const minTempDisplay = document.querySelector(
+      ".focused-weather-pane .temp-container .min-temp-display",
+    );
+    const maxTempDisplay = document.querySelector(
+      ".focused-weather-pane .temp-container .max-temp-display",
+    );
+
+    const descriptionDisplay = document.querySelector(
+      ".focused-weather-pane .other-info-container .weather-description",
+    );
+
+    const iconDisplay = document.querySelector(".focused-weather-pane img")
+
+    iconDisplay.src = rainIcon;
+    descriptionDisplay.innerText = today.description;
+    tempDisplay.innerText = today.temp;
+    minTempDisplay.innerText = today.tempmin;
+    maxTempDisplay.innerText = today.tempmax;
+  })();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "images/[hash][ext][query]",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist'
+    static: "./dist",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      }
-    ]
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/template.html'
-    })
-  ]
+      template: "./src/template.html",
+    }),
+  ],
 };
